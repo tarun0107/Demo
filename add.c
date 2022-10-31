85df4e5accf90a0a20b0d990af89d21363430b81
Tarun Tapadiya <69494660+tarun0107@users.noreply.github.com>
2022-10-31 12:08:01 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-int add (int a, int b) {
+int add (int a, int b, int c) {
 	return a + b;
 }
 int main() {
-	int a = 5, b = 6;
-        printf("%d\n", add(a, b));
+	int a = 5, b = 6, c = 7;
+        printf("%d\n", add(a, b, c));
 	return 0;
 }
